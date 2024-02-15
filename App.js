daab65b3ebfba985085f171da0ad8bf4a9f9e7c2
MalaminWaggeh <140919729+MalaminWaggeh@users.noreply.github.com>
2024-02-15 01:32:05 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignupForm'; // Updated import statement for SignupForm
 import Dashboard from './components/Dashboard';
+import AboutMe from './components/AboutMe'; 
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div>
-        <h1>Todo App</h1>
+      <div style={{
+        background: "url('https://coolbackgrounds.io/images/backgrounds/index/ranger-4df6c1b6.png') no-repeat center center fixed",
+        WebkitBackgroundSize: 'cover',
+        MozBackgroundSize: 'cover',
+        OBackgroundSize: 'cover',
+        backgroundSize: 'cover',
+        minHeight: '100vh', 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
           <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} /> {/* Render SignupForm for /signup route */}
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/aboutme" element={<AboutMe />} /> {/* Add the AboutMe route */}
+          {/* other routes... */}
         </Routes>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
